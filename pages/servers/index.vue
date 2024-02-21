c338cee8979df47c27e6d5d65fc7576875790e49
Yaroslav Bogdanov <jarosl.s@yandex.ru>
2024-02-21 16:38:31 +0300

    Добавлен функционал кнопкам списка серверов

--- pages/servers/index.vue
+++ pages/servers/index.vue
@@ -39,6 +39,9 @@ export default {
     },
   },
   methods: {
+    copyToClipboard(text){
+      navigator.clipboard.writeText(text);
+    },
     findChapterById, // Добавляем функцию в методы компонента
     togglePlayersList(serverId) {
       // Получаем текущее состояние для конкретного сервера
@@ -147,13 +150,15 @@ export default {
 <template>
   <div>
     <!-- Grid с серверной загруженностью -->
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 mx-2 md:mx-4 lg:mx-10 mt-4">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5
+                gap-4 py-16 px-8 my-8 mx-10 rounded-2xl backdrop-blur-md select-none"
+      id="grid-container">
       <!-- По серверная загруженность -->
-      <div v-for="(server, index) in serverData" :key="index" class="bg-white p-2 shadow-lg rounded-xl">
+      <div v-for="(server, index) in serverData" :key="index">
         <!-- Контейнер для изображения и текста -->
-        <div class=""> <!--flex flex-col md:flex-row relative-->
+        <div>
           <!-- Картинка карты -->
-          <div class="w-full object-cover h-24 overflow-hidden relative rounded-xl cursor-default" id="server-container">
+          <div class="w-full object-cover h-24 overflow-hidden relative rounded-xl cursor-default shadow-md" id="server-container">
             <img
                 :src="getServerMapImage(server.map)"
                 alt="Карта"
@@ -163,7 +168,7 @@ export default {
             <!-- Контент картинки -->
             <div class="m-3">
               <!-- Название сервера-->
-              <h2 class="text-lg font-semibold font_server absolute top-2 text-white">
+              <h2 class="text-lg font-semibold font_server absolute top-2 text-white select-text">
                 {{ getServerName(server.name)[0] }} {{ getServerName(server.name)[1] }}
                 <span v-if="isServerOffline(server.timestamp)" class="text-red-500 font_l4d4">Offline</span>
               </h2>
@@ -180,21 +185,27 @@ export default {
                   }"></div> 
                 </div>
                 <div>{{server.players.length}}/{{server.maxplayers}}</div>
-                <div>{{ server.map }}</div>
+                <div class="select-text">{{ server.map }}</div>
               </div>
               <!-- Кнопки -->
               <div class="absolute bottom-0 right-0 text-white h-24 w-12 flex flex-col justify-around">
                 <!-- Copy -->
-                <svg class="hover:bg-white hover:opacity-20 h-12 px-3 rounded-bl-xl"
-                  xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
-                  <path fill-rule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"/>
-                </svg>
+                <a
+                  @click="copyToClipboard(server.connect)"
+                  class="hover:bg-white hover:bg-opacity-20 h-12 p-3 rounded-bl-xl duration-100 active:bg-opacity-40 btn-with-icon">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                    <path class="icon-in-btn" fill-rule="evenodd" d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0"/>
+                    <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z"/> <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z"/>
+                  </svg>
+                </a>
                 <!-- Play -->
-                <svg 
-                  class="hover:bg-white hover:opacity-20 h-12 px-3 rounded-tl-xl"
-                  xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
-                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/><path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445"/>
-                </svg>
+                <a
+                  :href="'steam://connect/' + server.connect" target="_blank"
+                  class="hover:bg-white hover:bg-opacity-20 h-12 p-3 rounded-tl-xl duration-100 active:bg-opacity-40">
+                  <svg xmlns="http://www.w3.org/2000/svg" :class="{'fill-white':server.players.length!=server.maxplayers,'fill-gray-500':server.players.length==server.maxplayers}" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/><path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445"/>
+                  </svg>
+                </a>
               </div>
             </div>
           </div>
@@ -216,5 +227,19 @@ export default {
 {
   filter:brightness(100%);
   transition-property: filter;
+}
+#grid-container{
+  box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.3), 0 4px 6px -4px rgb(0 0 0 / 0.1);
+}
+
+.btn-with-icon:active .icon-in-btn{
+  fill-opacity: 100%;
+  transition-duration: 10ms;
+  transition-delay: 0ms;
+}
+.icon-in-btn{
+  fill-opacity: 0;
+  transition-delay: 2000ms;
+  transition-duration: 300ms;
 }
 </style>
