764ead288183895b5ba022edd227115fabc8060f
Yaroslav Bogdanov <jarosl.s@yandex.ru>
2024-02-21 14:38:22 +0300

    Прототип нового вида страницы серверов

--- pages/servers/index.vue
+++ pages/servers/index.vue
@@ -146,112 +146,56 @@ export default {
 
 <template>
   <div>
-    <!-- Общая загруженность -->
-    <div class="w-full max-w-screen-xl mx-auto mt-10 p-4 bg-white shadow-lg rounded-lg">
-      <!-- Полоса прогресса -->
-      <div class="relative pt-1 w-full">
-        <div class="flex h-7 mb-2 overflow-hidden text-xs bg-gray-300 rounded">
-          <div
-              :style="'width:' + Math.max((totalPlayers / totalMaxPlayers) * 100, 10) + '%'"
-              :class="{
-                'bg-green-400': totalPlayers <= 30,
-                'bg-yellow-400': totalPlayers > 30 && totalPlayers <= 55,
-                'bg-red-400': totalPlayers > 55
-               }"
-              class="flex flex-col text-center whitespace-nowrap text-white justify-center relative">
-            <!-- Текст с количеством игроков -->
-            <span class="absolute inset-0 flex items-center justify-center text-xl text-black">
-            {{ totalPlayers }}/{{ totalMaxPlayers }}
-          </span>
-          </div>
-        </div>
-      </div>
-    </div>
-
-
     <!-- Grid с серверной загруженностью -->
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-2 md:mx-4 lg:mx-10 mt-4">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 mx-2 md:mx-4 lg:mx-10 mt-4">
       <!-- По серверная загруженность -->
-      <div v-for="(server, index) in serverData" :key="index" class="bg-white p-2 shadow-lg rounded-lg">
+      <div v-for="(server, index) in serverData" :key="index" class="bg-white p-2 shadow-lg rounded-xl">
         <!-- Контейнер для изображения и текста -->
-        <div class="flex flex-col md:flex-row">
+        <div class=""> <!--flex flex-col md:flex-row relative-->
           <!-- Картинка карты -->
-          <div class="w-5/5 sm:w-7/12 md:mr-1 2xl:mr-4 md:overflow-hidden"> <!-- Увеличили ширину карточки на больших экранах -->
+          <div class="w-full object-cover h-24 overflow-hidden relative rounded-xl cursor-default" id="server-container">
             <img
                 :src="getServerMapImage(server.map)"
                 alt="Карта"
                 @error="handleImageError(server)"
-                class="w-full object-cover h-52 rounded-lg border-black border-2"
+                class="object-cover overflow-hidden w-full h-24 brightness-50 duration-200" id="server-image"
             />
-<!--            <p class="text-center mt-1 text-gray-600">{{ findChapterById(server.map).chapterName }}</p>-->
-            <p class="text-center mt-2 text-gray-600">Карта: <span class="text-green-900">{{ server.map }} | {{ findChapterById(server.map).chapterName }}</span></p>
-          </div>
-
-          <!-- Информация о сервере (справа от картинки) -->
-          <div class="w-full 2xl:w-1/3 md:w-3/5 flex flex-col"> <!-- Увеличили ширину карточки на больших экранах -->
-            <h2 class="text-lg md:text-2xl font-semibold font_server py-2 text-center">
-              {{ getServerName(server.name)[0] }}
-              <br>
-              {{ getServerName(server.name)[1] }}
-              <span v-if="isServerOffline(server.timestamp)" class="text-red-500 ml-2 font_l4d4">Offline</span>
-            </h2>
-
-            <!-- Полоса прогресса для игроков -->
-            <div class="relative pt-1">
-              <!-- Задний фон для полосы-->
-              <div class="bg-gray-300 h-7 flex inset-0 text-center whitespace-nowrap text-white rounded overflow-hidden">
-                <div
-                  :style="'width:' + Math.max((server.players.length / server.maxplayers) * 100, 0) + '%'"
+            <!-- Контент картинки -->
+            <div class="m-3">
+              <!-- Название сервера-->
+              <h2 class="text-lg font-semibold font_server absolute top-2 text-white">
+                {{ getServerName(server.name)[0] }} {{ getServerName(server.name)[1] }}
+                <span v-if="isServerOffline(server.timestamp)" class="text-red-500 font_l4d4">Offline</span>
+              </h2>
+              <!-- Карта и онлайн -->
+              <div class="text-gray-400 absolute bottom-2 font-semibold justify-between flex space-x-2 text-md">
+                <!-- Прогресс игроков-->
+                <div class="bg-gray-500 rounded-sm overflow-hidden w-2 full-height rotate-180">   
+                  <div
+                  :style="'height:' + Math.max((server.players.length / server.maxplayers) * 100, 0) + '%'"
                   :class="{
                     'bg-green-400': server.players.length <= 4,
                     'bg-yellow-400': server.players.length > 4 && server.players.length <= 6,
                     'bg-red-400': server.players.length > 6
-                  }"
-                  class="h-7 flex flex-col text-center whitespace-nowrap text-white justify-center relative">
+                  }"></div> 
                 </div>
-                <!-- Текст с количеством игроков -->
-                <span class="absolute inset-0 flex items-center justify-center text-lg text-black font-bold">
-                    {{ server.players.length }}/{{ server.maxplayers }}
-                </span>
+                <div>{{server.players.length}}/{{server.maxplayers}}</div>
+                <div>{{ server.map }}</div>
               </div>
-            </div>
-
-            <!-- Кнопка "Показать игроков" -->
-            <button class="text-blue-500 cursor-pointer text-lg mt-1 py-1" @click="togglePlayersList(server.serverId)">
-              Показать игроков
-            </button>
-
-            <!-- Таблица для списка игроков -->
-            <table v-if="serverStates.get(String(server.serverId))" class="text-sm mt-2 w-full">
-              <thead>
-              <tr>
-                <th>Имя игрока</th>
-                <th>Время</th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr v-for="(player, playerIndex) in server.players" :key="playerIndex" class="player-row">
-                <td class="text-black text-lg">{{ player.name }}</td>
-                <td class="text-xs">{{ formattedTime(player.raw.time) }}</td>
-              </tr>
-              </tbody>
-            </table>
-
-            <!-- Адрес сервера и кнопка "Подключиться" -->
-            <div class="mt-2 text-g">
-              <p class="py-1 font-sans">IP: {{ server.connect }}</p>
-              <a
-                  :href="'steam://connect/' + server.connect"
-                  target="_blank"
-                  :class="{
-                'inline-block mt-3 text-md px-8 py-3 bg-green-500 text-gray-100 rounded-full hover:bg-blue-600 transition duration-300 ease-in-out': server.players.length <= 3,
-                'inline-block mt-3 text-md px-8 py-3 bg-yellow-500 text-gray-100 rounded-full hover:bg-blue-600 transition duration-300 ease-in-out': server.players.length > 3 && server.players.length <= 6,
-                'inline-block mt-3 text-md px-8 py-3 bg-red-500 text-gray-100 rounded-full hover:bg-blue-600 transition duration-300 ease-in-out': server.players.length > 6,
-                'inline-block mt-3 text-md px-8 py-3 bg-zinc-800 text-gray-500 rounded-full hover:bg-blue-600 transition duration-300 ease-in-out': server.players.length === 8,
-              }"
-              >
-                🔗Подключиться
-              </a>
+              <!-- Кнопки -->
+              <div class="absolute bottom-0 right-0 text-white h-24 w-12 flex flex-col justify-around">
+                <!-- Copy -->
+                <svg class="hover:bg-white hover:opacity-20 h-12 px-3 rounded-bl-xl"
+                  xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"/>
+                </svg>
+                <!-- Play -->
+                <svg 
+                  class="hover:bg-white hover:opacity-20 h-12 px-3 rounded-tl-xl"
+                  xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/><path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445"/>
+                </svg>
+              </div>
             </div>
           </div>
         </div>
@@ -264,22 +208,13 @@ export default {
 
 
 <style>
-.player-row {
-  position: relative; /* Сделать позицию относительной, чтобы можно было добавить псевдоэлемент */
-}
-
-.player-row::after {
-  content: ''; /* Псевдоэлемент для создания полоски */
-  position: absolute;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  height: 2px; /* Высота полоски */
-  background-color: #096600; /* Цвет полоски */
-}
-
-
 .font_server {
   font-family: 'din';
+}
+
+#server-container:hover #server-image
+{
+  filter:brightness(100%);
+  transition-property: filter;
 }
 </style>
